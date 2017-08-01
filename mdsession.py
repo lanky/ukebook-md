4e25c159bc754a5d37a1a9c0a7c2aba019403a4e
Stuart Sears <stuart@sjsears.com>
2017-08-02 00:02:07 +0100

    Updated mdsession to be commandline testing tool
    - now uses jinja2 for templating

--- mdsession.py
+++ mdsession.py
@@ -1,19 +1,48 @@
 #!/usr/bin/env python 
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 import markdown
 import codecs
 import ukedown_extension
-input_file = codecs.open("inputs/dontstrop.txt", mode="r", encoding="utf-8")
-txt = input_file.read()
+from jinja2 import Environment, FileSystemLoader
 
-default = markdown.markdown(txt)
-# adding BR tags to newlines
-br = markdown.markdown(txt, extensions=['markdown.extensions.nl2br'])
-# attempting to surround chords with <span class='chord'>XXX</span>
-chordext = ukedown_extension.ChordExtension()
-blockext = ukedown_extension.BoxExtension()
+import sys
+import os
 
-brcd = markdown.markdown(txt, extensions=['markdown.extensions.nl2br', chordext, blockext])
+def render(template, context):
+    j2env = Environment(loader=FileSystemLoader("templates"))
 
-print(brcd)
+    tpl = j2env.get_template(template)
+
+    return tpl.render(context)
+
+def ukedown_to_html(inputfile):
+
+    ukebook = ukedown_extension.UkeBookExtension()
+    
+    fh = codecs.open(inputfile, mode="r", encoding="utf-8")
+    txt = fh.read()
+
+    return markdown.markdown(txt, extensions=['markdown.extensions.nl2br', ukebook])
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv[1:]) == 0:
+        print ("no filename")
+        sys.exit(1)
+    else:
+        for f in sys.argv[1:]:
+            try:
+                ud = ukedown_to_html(f)
+                out = render("ukesong.j2", {'rendered_text': ud })
+                print(out.encode("utf-8", "ignore"))
+
+            except (IOError, OSError), E:
+                print ("oops - couldn't render %s (%s)" % (E.filename, E.strerror))
+
+            except:
+                # all other errors
+                raise
+
 
