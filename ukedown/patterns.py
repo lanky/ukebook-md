c727897afbc48b98a5586e76e1fdb72190d3cca5
Stuart Sears <stuart@sjsears.com>
2017-08-15 19:51:10 +0100

    split out regex and translation tables

---
+++ ukedown/patterns.py
@@ -0,0 +1,28 @@
+## PATTERNS FOR UKEDOWN ##
+# These are all python regular expressions, RTFM if needed
+# when used that will probably be compiled thus:
+# re.compile(PATTERN, re.UNICODE|re.DOTALL)
+
+# approximation of chords - covers major minor dim aug sus and 7/9/13 etc
+CHORD = ur'\(([A-G][adgijmnsu0-9#b+-]*(?:\s*[\u2013\u2014-]\s*single(?:\s*strums?)?)?)\)'
+
+DASHES = ur'\u2010\u2011\u2012\u2013\u2014'
+
+# used to split title and artist from our first line
+HYPHENS = ur'\s*(?:-|\u2011|\u2013|\u2014|\u2015)\s*'
+
+# backing vox - anything in () that is not a chord.
+VOX = r'\(([\w\s]+)\)'
+
+# band/performance instructions - use a differnt delimiter - {}
+NOTES = r'\{([\w\s]+)\}'
+
+# lines that start (and optionally end) with a | character are 
+# part of boxed paragraphs
+BOX = r'(^|\n)\| *([^ ][^|]*)\|?$'
+
+# a line containing something encapsulated by [] characters
+HEADER = r'^(.*)\[([^]]+)\](.*)$'
+
+# pattern to pickup and "boldify" repetitions
+REPEATS = r'(x\d+)'
---
+++ ukedown/translations.py
@@ -0,0 +1,24 @@
+## part of ukedown ##
+# patterns for  translating all the horrible 'smart' characters that word etc like to 
+# put in when you just want boring hyphens or quotes or brackets (or whatever)
+# basically  this is a dictionary mapping ord(UNICODE) to UNICODE_REPLACEMENT
+from __future__ import unicode_literals
+
+WP_JUNK = {
+        # first hyphen characters, there are a few and wordprocessors do love 'em
+        '\u2010': '-', # hyphen
+        '\u2011': '-', # non-breaking hyphen
+        '\u2012': '-', # figure-dash
+        '\u2013': '-', # en-dash
+        '\u2014': '-', # em-dash
+        '\u2015': '-', # horizontal bar
+        # then the silly pretty quote things): let's just use the normal ascii ones
+        '\u2018': "'", # left single quotation
+        '\u2019': "'", # right single quotation
+        '\u201c': '"', # left double quotation
+        '\u201d': '"', # right double quotation
+        }
+
+# unicode strings have their own 'translate' method so we just need a table:
+UNICODE_CLEAN_TABLE = dict((ord(k), v) for k,v in WP_JUNK.items())
+
