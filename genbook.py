2667cfc2077d438a853addde47739aa72b740063
Stuart Sears <stuart@sjsears.com>
2019-03-02 12:36:55 +0000

    refactored book generation
    - gathers metadata
    - generates inline chords

--- genbook.py
+++ genbook.py
@@ -4,6 +4,8 @@
 
 import markdown
 import ukedown.udn
+# local chord generation tool (SVGs)
+import chordgen
 
 # jinja2 templating, originially based one the django model.
 from jinja2 import Environment, FileSystemLoader
@@ -17,9 +19,14 @@ import os
 import datetime
 import shutil
 import codecs
+import yaml
+import re
+
+from progress.bar import Bar
 
 import argparse
 from glob import glob
+
 
 def parse_commandline(argv):
     """
@@ -33,9 +40,12 @@ def parse_commandline(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="name of input directory")
     parser.add_argument("-s", "--style", help="output style, must correspond to a stylesheet in the css dir", choices=['weds', 'belfast', 'karauke', 'singers'], default='weds')
-    parser.add_argument("-o", "--output", help="Name of Book to build (default is Karauke_YYYY_MM_DD", default="Karauke_{:%Y-%m-%d}".format(datetime.datetime.now()))
+    parser.add_argument("-o", "--output", default="Karauke_{:%Y-%m-%d}".format(datetime.datetime.now()),
+        help="Name of Book to build (default is Karauke_YYYY_MM_DD" )
     parser.add_argument("--stylesheet", help="provide a custom stylesheet to test rendering. Expects to be given a path.")
     parser.add_argument("--template", help="provide a custom template to test rendering. Expects to be given a path")
+    parser.add_argument("--report", action="store_true", default=False,
+        help="Output a report on input directory and available chords. For info only")
 
     args = parser.parse_args(argv)
 
@@ -49,6 +59,15 @@ def parse_commandline(argv):
         print("Output directory {0.output} already exists. Will replace files in it".format(args))
 
     return args
+
+def safe_name(chord):
+    # rules:
+    # replace '#' with _sharp if at end,
+    #                   _sharp_ if not
+    # replace '/' with _on_
+    return chord.translate({ ord('#'): '_sharp_', ord('/'): '_on_'})
+
+
 
 def render(template, context, template_dir="templates"):
     """
@@ -75,96 +94,151 @@ def ukedown_to_html(inputfile):
 
     return markdown.markdown(txt, extensions=['markdown.extensions.nl2br', 'ukedown.udn'])
 
+def create_layout(destdir, *subdirs):
+    """
+    Basic wrapper around os.makedirs to create our cnntainer layout
+    """
+    if not os.path.isdir(destdir):
+        try:
+            os.makedirs(destdir)
+        except (IOError, OSError) as E:
+            print ("Unable to create dir {0.filename} ({0.strerror}".format(E))
+            sys.exit(1)
+    for sd in subdirs:
+        d = os.path.join(destdir, sd)
+        if os.path.isdir(d):
+            continue
+        print ("creating {}".format(d))
+        try:
+            os.makedirs(d)
+        except (IOError, OSError) as E:
+            print ("Unable to create dir {0.filename} ({0.strerror}".format(E))
+            sys.exit(1)
+
+def analyse_inputs(inputdir):
+    """
+    Processes songsheets, returns a context (dict) containing
+    song: { id: NNN, title: X, artist: X, chords: [X],
+    """
+
+    songs = glob(os.path.join(inputdir, "*.udn"))
+
+    context = {'chords': set([]), 'songs': []}
+    pbar = Bar("Analysing Content: ", max=len(songs))
+    for sng in sorted(songs):
+        # index for nav documents/object ids
+        song_id = "{:03d}".format(pbar.index + 1)
+        # basename of outputfile
+        song_dest = os.path.basename(re.sub(r'\.udn$', '.html', sng))
+
+        # convert song body to html
+        content = ukedown_to_html(sng)
+        # process with bs4
+        soup = bs(content, features="lxml")
+
+        # strip out title/artist, if there is one
+        hdr = soup.h1.extract()
+        try:
+            title, artist = [ i.strip() for i in hdr.text.split('-', 1) ]
+        except ValueError:
+            title = hdr.text.strip()
+
+        hdr.decompose()
+
+        # Now process chords
+        # funky set comprehensions ftw
+        songchords = { c.text.split().pop(0) for c in soup.findAll('span', {'class': 'chord'}) }
+        # add our chords to the global chordlist
+        context['chords'].update(songchords)
+
+        context['songs'].append({ 'filename': song_dest,
+                                  'id': song_id,
+                                  'artist': artist.strip(),
+                                  'title': title.strip(),
+                                  'chords': [ safe_name(c) for c in songchords],
+                                  'html': str(soup)
+                                  })
+        pbar.next()
+
+    pbar.finish()
+    return context
+
+
+def build_songbook():
+    # render templates for songs
+    pass
+
 def main(options):
     """
     main script entrypoint, expects an 'options' object from argparse.ArgumentParser
     """
-    css_dir = os.path.join(options.output, 'css')
-    chord_dir = os.path.join(options.output, 'chords')
-    # setup our output directory
+    index = {'songbook': opts.output,
+             'songlist': [],
+             'chordlist': set([]),
+             'stylesheets': [],
+             'missing': set([])}
 
-    for d in [ css_dir, chord_dir]:
-        if not os.path.isdir(d):
-            os.makedirs(d)
+
+    context = analyse_inputs(options.input)
+
+    # now generate the chord images from templates
+    with open('chords.yml') as cd:
+        chord_defs = yaml.safe_load(cd)
+
+    missing_chords = chordgen.generate(context['chords'], chord_defs, destdir="templates/svg", template="inline_svg.j2")
+
+    # now we need to create our EPUB layout
+    create_layout(options.output, 'EPUB', 'EPUB/css', 'EPUB/images', 'EPUB/songs', 'META-INF' )
+    css_dir = os.path.join(options.output, 'EPUB','css')
+    chord_dir = os.path.join(options.output, 'EPUB', 'chords')
+
+    # copy styles and templates in
+    shutil.copy2('templates/container.xml', os.path.join(options.output, 'META-INF'))
 
     for stylesheet in glob('css/*.css'):
         shutil.copy2(stylesheet, css_dir)
+        index['stylesheets'].append(os.path.basename(stylesheet))
+
+    for img in glob('images/*'):
+        shutil.copy2(img, os.path.join(options.output, 'EPUB', 'images'))
+
+    env = Environment(loader=FileSystemLoader('templates'))
+    tpl = env.get_template('ukesong_inline.j2')
+
+    # now let's generate our songsheets
+    for songobj in Bar("Rendering").iter(context['songs']):
+        with open(os.path.join(options.output, 'EPUB', 'songs', songobj['filename']), 'w') as sf:
+            sf.write(tpl.render(songobj))
 
 
 
-    # build an index object
-    index = {'songbook': opts.output, 'songlist': []}
-
-    songs = glob("%s/*.udn" % options.input)
-
-    for f in sorted(songs):
-        print ("processing ", f)
+    if not options.report:
         try:
-            idx_entry = {}
-            ctx = {}
-            html = ukedown_to_html(f)
-            # process that with bs4? Find the classes you require?
-            # to generate metadata?
-            soup = bs(html, features="lxml")
-            # find the chords and associate them with diagrams
-            chords = []
-            for crd in [c.text.split().pop(0) for c in soup.findAll('span', {'class': 'chord'})]:
-                # we only want the first word
-                c = crd.split().pop(0)
-                # munge filenames, '/' -> 'slash',
-                # '#' -> 'sharp'
-                if '/' in c:
-                    c.replace('/', 'slash')
-                if '#' in c:
-                    c.replace('#', 'sharp')
-                if c not in chords:
-                    chords.append(c)
-            ctx['chords'] = chords
-            for crd in chords:
-                crdfile = "{}.gif".format(crd)
-                if not os.path.isfile("chords/{}".format(crdfile)):
-                    print("Missing chord diagram {}".format(crdfile))
-                    continue
-                if not os.path.isfile(os.path.join(chord_dir, crdfile)):
-                    shutil.copy2("chords/{}".format(crdfile), chord_dir)
-
-            # extract first h1 tag and make it the document title, with optional artist
-            # there should only be one of these anyway, even if not, we'll extract the first as song title
-            hdr = soup.h1.extract()
-            try:
-                if '-' in hdr.text:
-                    ctx['title'], ctx['artist'] = [i.strip() for i in hdr.text.split('-', 1) ]
-                else:
-                    ctx['title'] = hdr.text.strip()
-            except ValueError:
-                print ("Cannot parse {}".format(hdr.text))
-
-            # remove it from the document, we'll render it differently
-            hdr.decompose()
-
-            # print(ctx)
-            ctx['html'] = str(soup)
-
-            out = render("ukesong.j2", ctx)
-            output = "{}.html".format(os.path.splitext(os.path.basename (f))[0])
-            idx_entry = { 'filename': output,
-                          'artist': ctx.get('artist'),
-                          'title': ctx.get('title')
-                          }
-            with codecs.open(os.path.join(options.output, output), mode='wb', encoding="utf-8") as fd:
-                fd.write(out)
-
-        except (IOError, OSError) as E:
-            print ("oops - couldn't render %s (%s)" % (E.filename, E.strerror))
-
+            indexpg = render('nav.xhtml.j2', index)
+            with open(os.path.join(opts.output, 'EPUB/nav.xhtml'), 'w') as index_out:
+                index_out.write(indexpg)
+            with open(os.path.join(opts.output, 'EPUB/package.opf'), 'w') as pkgfile:
+                pkgfile.write(render('package.opf.j2', index))
+            with open(os.path.join(opts.output, 'EPUB/songs/index.html'), 'w') as idxfile:
+                idxfile.write(render('bookindex.j2', index))
         except:
-            # all other errors
+            print (yaml.safe_dump(index))
             raise
-        index['songlist'].append(idx_entry)
+    if options.report:
+        print ("""
+        Songook Summary for: {0.output}
+        input directory: {0.input}
+        Song Count: {1}
+        Chords Used: {2}
+        Missing Chord Definitions
+        {3}""".format(options,
+                      len(index['songlist']),
+                      ','.join(index['chordlist']),
+                      ','.join(sorted(list(index['missing']))) ))
 
-    indexpg = render('bookindex.j2', index)
-    with open(os.path.join(opts.output, 'index.html'), 'w') as index_out:
-        index_out.write(indexpg)
+
+
+
 
 
 
