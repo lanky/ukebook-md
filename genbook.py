30a1db755da46bcb7ce24def76a62be585cdb3db
Stuart Sears <stuart@sjsears.com>
2019-03-07 13:53:50 +0000

    refactored genbook tool
    - creates EPUB structures
    - templates using inline svg
    - moved to a 3-stage process, parse, gen chords, template.

--- genbook.py
+++ genbook.py
@@ -8,7 +8,8 @@ import ukedown.udn
 import chordgen
 
 # jinja2 templating, originially based one the django model.
-from jinja2 import Environment, FileSystemLoader
+# from jinja2 import Environment, FileSystemLoader
+import jinja2
 
 # for generating summary info
 from bs4 import BeautifulSoup as bs
@@ -21,11 +22,15 @@ import shutil
 import codecs
 import yaml
 import re
+from operator import itemgetter
+import logging
 
 from progress.bar import Bar
 
 import argparse
 from glob import glob
+
+logging.basicConfig(format="%(asctime)s %(levelname)-8s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", filename="bookmaker.log", level=logging.DEBUG)
 
 
 def parse_commandline(argv):
@@ -34,20 +39,23 @@ def parse_commandline(argv):
     """
     preamble = """
     Process a directory of ukedown-formatted song sheets to create an HTML-formatted book.
-    This will process all files found in the given directory, converting them to HTML, inserting chord diagrams (can be hidden with CSS)
+    This will process all files found in the given directory, converting them to HTML,
+    inserting chord diagrams (can be hidden with CSS)
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("input", help="name of input directory")
-    parser.add_argument("-s", "--style", help="output style, must correspond to a stylesheet in the css dir", choices=['weds', 'belfast', 'karauke', 'singers'], default='weds')
+    parser.add_argument("input", help="name of input directory", nargs="*")
+    parser.add_argument("-s", "--style", choices=['weds', 'belfast', 'karauke', 'singers'], default='weds',
+        help="output style, must correspond to a stylesheet in the css dir")
     parser.add_argument("-o", "--output", default="Karauke_{:%Y-%m-%d}".format(datetime.datetime.now()),
         help="Name of Book to build (default is Karauke_YYYY_MM_DD" )
-    parser.add_argument("--stylesheet", help="provide a custom stylesheet to test rendering. Expects to be given a path.")
-    parser.add_argument("--template", help="provide a custom template to test rendering. Expects to be given a path")
     parser.add_argument("--report", action="store_true", default=False,
         help="Output a report on input directory and available chords. For info only")
 
     args = parser.parse_args(argv)
+
+    if len(args.input) == 0:
+        args.input = ['inputs']
 
     if not os.path.isdir(args.output):
         try:
@@ -81,7 +89,7 @@ def render(template, context, template_dir="templates"):
         template_dir(str): where to look for templates, default is the local 'templates' directory.
 
     """
-    j2env = Environment(loader=FileSystemLoader(template_dir))
+    j2env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
 
     tpl = j2env.get_template(template)
 
@@ -115,24 +123,33 @@ def create_layout(destdir, *subdirs):
             print ("Unable to create dir {0.filename} ({0.strerror}".format(E))
             sys.exit(1)
 
-def analyse_inputs(inputdir):
+def parse_songsheets(inputdirs):
     """
     Processes songsheets, returns a context (dict) containing
     song: { id: NNN, title: X, artist: X, chords: [X],
     """
-
-    songs = glob(os.path.join(inputdir, "*.udn"))
+    songs = {}
+    # will merge dirs together, if a song appears twice, last match wins
+    for idir in inputdirs:
+        if not os.path.isdir(idir):
+            continue
+        songs.update({ os.path.basename(s): os.path.realpath(s) for s in glob(os.path.join(idir, '*.udn')) })
 
     context = {'chords': set([]), 'songs': []}
-    pbar = Bar("Analysing Content: ", max=len(songs))
-    for sng in sorted(songs):
+    pbar = Bar("Analysing Content: ".ljust(20), max=len(songs))
+    # This will sort items across multiple directories
+    for sng, path in sorted(songs.items(), key=itemgetter(0)):
         # index for nav documents/object ids
+        prev_id = "{:03d}".format(pbar.index)
         song_id = "{:03d}".format(pbar.index + 1)
+        next_id = "{:03d}".format(pbar.index + 2)
+
+
         # basename of outputfile
-        song_dest = os.path.basename(re.sub(r'\.udn$', '.html', sng))
+        song_dest = re.sub(r'\.udn$', '.html', sng)
 
         # convert song body to html
-        content = ukedown_to_html(sng)
+        content = ukedown_to_html(path)
         # process with bs4
         soup = bs(content, features="lxml")
 
@@ -153,12 +170,15 @@ def analyse_inputs(inputdir):
 
         context['songs'].append({ 'filename': song_dest,
                                   'id': song_id,
+                                  'prev_id': prev_id,
+                                  'next_id': next_id,
                                   'artist': artist.strip(),
                                   'title': title.strip(),
                                   'chords': [ safe_name(c) for c in songchords],
-                                  'html': str(soup)
+                                  'html': ''.join([str(x) for x in soup.body.contents ])
                                   })
         pbar.next()
+        context['index'] = { s['id']: s['filename'] for s in context['songs'] }
 
     pbar.finish()
     return context
@@ -172,23 +192,50 @@ def main(options):
     """
     main script entrypoint, expects an 'options' object from argparse.ArgumentParser
     """
+    logging.info("Book Generation Started at {:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now()))
     index = {'songbook': opts.output,
              'songlist': [],
              'chordlist': set([]),
              'stylesheets': [],
              'missing': set([])}
+    # context  object looks like
+    # { chords: set(),
+    #   songs: [ { filename: str,
+    #              id: 0-padded str(int)
+    #              artist
+    #              title
+    #              chords: list/str
+    #              html: body as html
+    #            } ]
+    #   stylesheets: list, added below
+    #   songbook: output dir
+    #   images: list of filenames
+    #   sources: list of directories containing songsheets
+    # }
 
 
     # context created by analysing input files and options:
-    context = analyse_inputs(options.input)
+    context = parse_songsheets(options.input)
     context['songbook'] = os.path.basename(options.output)
     context['stylesheets'] = []
     context['images'] = []
 
-
-    # now generate the chord images from templates
     with open('chords.yml') as cd:
         chord_defs = yaml.safe_load(cd)
+
+    if options.report:
+        print ("""
+        Songook Summary for: {0.songbook}
+        input directory: {0.input}
+        Song Count: {1}
+        Chords Used: {2}
+        Missing Chord Definitions
+        {3}""".format(options,
+                      len(context['songs']),
+                      ','.join(context['chordlist']),
+                      ','.join(sorted(list(context['missing']))) ))
+
+    # now generate the chord images from templates
 
     missing_chords = chordgen.generate(context['chords'], chord_defs, destdir="templates/svg", template="inline_svg.j2")
 
@@ -208,55 +255,41 @@ def main(options):
         shutil.copy2(img, os.path.join(options.output, 'EPUB', 'images'))
         context['images'].append(img)
 
-    env = Environment(loader=FileSystemLoader('templates'))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'), trim_blocks=True)
+
 
     # now let's generate our songsheets
-    tpl = env.get_template('ukesong_inline.j2')
-    for songobj in Bar("Rendering").iter(context['songs']):
-        with open(os.path.join(options.output, 'EPUB', 'songs', songobj['filename']), 'w') as sf:
-            sf.write(tpl.render(songobj))
-
-    # now generate the other templates for epub
-    tpl = 'nav.xhtml.j2'
-    with open(os.path.join(options.output, 'EPUB', 'nav.xhtml'), 'w') as nav:
-        nav.write(tpl.render(context))
-
-    tpl = 'product.opf.j2'
-    with open(os.path.join(options.output, 'EPUB', 'product.opf'), 'w') as prod:
-        prod.write(tpl.render(context))
-
-
-
-
-    if not options.report:
+    songtemplate = env.get_template('ukesong_inline.j2')
+    failures = []
+    # so, calc prev and next...
+    # simples, generate a   quick index...
+    for songobj in Bar("Rendering Songs:".ljust(20)).iter(context['songs']):
+        logging.info("rendering {artist} - {title} into {filename}".format(**songobj))
+        logging.debug("Chords: {chords!r}".format(**songobj))
+        songobj['_prev'] = context['index'].get(songobj['prev_id'], "../index/html")
+        songobj['_next'] = context['index'].get(songobj['next_id'], "../index/html")
         try:
-            indexpg = render('nav.xhtml.j2', index)
-            with open(os.path.join(opts.output, 'EPUB/nav.xhtml'), 'w') as index_out:
-                index_out.write(indexpg)
-            with open(os.path.join(opts.output, 'EPUB/package.opf'), 'w') as pkgfile:
-                pkgfile.write(render('package.opf.j2', index))
-            with open(os.path.join(opts.output, 'EPUB/songs/index.html'), 'w') as idxfile:
-                idxfile.write(render('bookindex.j2', index))
-        except:
-            print (yaml.safe_dump(index))
-            raise
-    if options.report:
-        print ("""
-        Songook Summary for: {0.output}
-        input directory: {0.input}
-        Song Count: {1}
-        Chords Used: {2}
-        Missing Chord Definitions
-        {3}""".format(options,
-                      len(index['songlist']),
-                      ','.join(index['chordlist']),
-                      ','.join(sorted(list(index['missing']))) ))
+            with open(os.path.join(options.output, 'EPUB', 'songs', songobj['filename']), 'w') as sf:
+                sf.write(songtemplate.render(songobj, songidx=context['index']))
+        except jinja2.TemplateError as T:
+            logging.exception("Failed to render template for {title} - {artist}".format(**songobj))
+            logging.error("Context: {chords!r}".format(**songobj))
+            failures.append((songobj, T))
+    for f, err in failures:
+        print ("{title} - {artist} -> {filename}".format(**f), err.__class__, err)
 
 
+    # other EPUB structures
+    template_maps = {
+        'EPUB/nav.xhtml': 'nav.xhtml.j2',
+        'EPUB/index.html': 'bookindex.j2',
+        'EPUB/package.opf': 'package.opf.j2'
+        }
 
-
-
-
+    for fpath, ftemplate in Bar("EPUB Templates: ".ljust(20)).iter(template_maps.items()):
+        t = env.get_template(ftemplate)
+        with open(os.path.join(options.output, fpath), 'w') as dest:
+            dest.write(t.render(context))
 
 if __name__ == "__main__":
     opts = parse_commandline(sys.argv[1:])
