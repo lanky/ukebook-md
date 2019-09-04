dcf7a9833f6564841708fc5a45725ce1774fb1d3
Stuart Sears <stuart@sjsears.com>
2019-09-04 08:13:12 +0100

    removed ukebook dirs (class work in "songbook")

--- ukebook/song.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# class wrapper around song objects
-
-class Song(object):
-    """
-    Represents a song, surprisingly
-    Handles parsing of chordnames, sections etc.
-    """
-
